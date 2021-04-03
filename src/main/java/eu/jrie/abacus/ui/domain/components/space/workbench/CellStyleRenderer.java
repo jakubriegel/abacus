c1e608c6de04fa3d57931546ddfc2158d47e2e39
jakubriegel <jakubriegel112@gmail.com>
2021-04-03 10:49:03 +0200

    basic-formulas | CellStyleRenderer

---
+++ src/main/java/eu/jrie/abacus/ui/domain/components/space/workbench/CellStyleRenderer.java
@@ -0,0 +1,72 @@
+package eu.jrie.abacus.ui.domain.components.space.workbench;
+
+import eu.jrie.abacus.core.domain.cell.Position;
+import eu.jrie.abacus.core.domain.cell.style.CellStyle;
+import eu.jrie.abacus.core.domain.cell.style.CellStyleProvider;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import java.awt.*;
+import java.awt.font.TextAttribute;
+import java.util.Map;
+
+import static java.awt.Font.BOLD;
+import static java.awt.Font.ITALIC;
+import static java.awt.Font.PLAIN;
+import static java.awt.font.TextAttribute.UNDERLINE;
+import static java.awt.font.TextAttribute.UNDERLINE_ON;
+
+public class CellStyleRenderer extends DefaultTableCellRenderer {
+
+    private final CellStyleProvider styleProvider;
+
+    public CellStyleRenderer(CellStyleProvider styleProvider) {
+        this.styleProvider = styleProvider;
+    }
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        var cell = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+        applyStyle(cell, row, column);
+        return cell;
+    }
+
+    private CellStyle getStyle(int row, int column) {
+        var position = new Position(column - 1, row);
+        return styleProvider.getStyle(position);
+    }
+
+    private void applyStyle(Component cell, int row, int column) {
+        var style = getStyle(row, column);
+        applyStyle(cell, style);
+
+    }
+
+    private static void applyStyle(Component cell, CellStyle style) {
+        applyBoldAndItalic(cell, style);
+        applyUnderline(cell, style);
+        applyBackground(cell, style);
+    }
+
+    private static void applyBoldAndItalic(Component cell, CellStyle style) {
+        var fontStyle = PLAIN + (style.isBold() ? BOLD : 0) + (style.isItalic() ? ITALIC : 0);
+        cell.setFont(cell.getFont().deriveFont(fontStyle));
+    }
+
+    private static void applyUnderline(Component cell, CellStyle style) {
+        var font = cell.getFont();
+        @SuppressWarnings("unchecked")
+        var attributes = (Map<TextAttribute, Object>) font.getAttributes();
+        if (style.isUnderlined()) {
+            attributes.put(UNDERLINE, UNDERLINE_ON);
+        } else {
+            attributes.remove(UNDERLINE);
+        }
+        cell.setFont(font.deriveFont(attributes));
+    }
+
+    private static void applyBackground(Component cell, CellStyle style) {
+        cell.setBackground(style.backgroundColor());
+    }
+
+}
---
+++ src/test/java/eu/jrie/abacus/ui/domain/components/space/workbench/CellStyleRendererTest.java
@@ -0,0 +1,91 @@
+package eu.jrie.abacus.ui.domain.components.space.workbench;
+
+import eu.jrie.abacus.core.domain.cell.Position;
+import eu.jrie.abacus.core.domain.cell.style.CellStyle;
+import eu.jrie.abacus.core.domain.cell.style.CellStyleProvider;
+import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
+
+import static java.awt.Color.red;
+import static java.awt.Font.BOLD;
+import static java.awt.Font.ITALIC;
+import static java.awt.Font.PLAIN;
+import static java.awt.font.TextAttribute.UNDERLINE;
+import static java.awt.font.TextAttribute.UNDERLINE_ON;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class CellStyleRendererTest {
+    private final CellStyleProvider styleProvider = mock(CellStyleProvider.class);
+
+    private final CellStyleRenderer renderer = new CellStyleRenderer(styleProvider);
+
+    @Test
+    void shouldApplyStyle() {
+        // given
+        var position = new Position(1, 2);
+        var column = position.x() + 1;
+        var row = position.y();
+
+        // and
+        var style = new CellStyle(true, true, true, red);
+        when(styleProvider.getStyle(position)).thenReturn(style);
+
+        // when
+        var result = renderer.getTableCellRendererComponent(new JTable(), null, false, false, row, column);
+
+        // then
+        verify(styleProvider).getStyle(position);
+
+        // and
+        assertEquals(BOLD + ITALIC, result.getFont().getStyle());
+        assertEquals(UNDERLINE_ON, result.getFont().getAttributes().get(UNDERLINE));
+        assertEquals(style.backgroundColor(), result.getBackground());
+    }
+
+    @Test
+    void shouldNotApplyBoldAndItalic() {
+        // given
+        var position = new Position(1, 2);
+        var column = position.x() + 1;
+        var row = position.y();
+
+        // and
+        var style = new CellStyle(false, false, true, red);
+        when(styleProvider.getStyle(position)).thenReturn(style);
+
+        // when
+        var result = renderer.getTableCellRendererComponent(new JTable(), null, false, false, row, column);
+
+        // then
+        verify(styleProvider).getStyle(position);
+
+        // and
+        assertEquals(PLAIN, result.getFont().getStyle());
+    }
+
+    @Test
+    void shouldNotApplyUnderline() {
+        // given
+        var position = new Position(1, 2);
+        var column = position.x() + 1;
+        var row = position.y();
+
+        // and
+        var style = new CellStyle(false, false, false, red);
+        when(styleProvider.getStyle(position)).thenReturn(style);
+
+        // when
+        var result = renderer.getTableCellRendererComponent(new JTable(), null, false, false, row, column);
+
+        // then
+        verify(styleProvider).getStyle(position);
+
+        // and
+        assertNull(result.getFont().getAttributes().get(UNDERLINE));
+    }
+}
